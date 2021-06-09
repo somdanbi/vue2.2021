6cf36ab3a94855a11b4bf8aed581dee678cd19dc
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-06-09 06:22:18 -0500

    Hide modal with btn

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 Vue.component('message', {
     props: ['title','body'],
 
+    data() {
+        return {
+            isVisible : true
+        }
+    },
+
+
+
     template:
-        `<article class="message">
+        `<article class="message" v-show="isVisible">
             <div class="message-header">
                 {{title}}
+                <button type="button" @click="hideModal" class="delete" aria-label="delete">x</button>
             </div>
             
             <div class="message-body">
                 {{body}}
             </div>
-        </article>`
+        </article>`,
+
+        methods: {
+        hideModal: function() {
+            this.isVisible = false;
+        }
+    }
+
+
+
+
 });
 
 
