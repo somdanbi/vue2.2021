f75b3fcdf6ac2ab9780b56a6d3a6acae98038248
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-06-09 07:24:20 -0500

    dinamyc modal text

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 Vue.component('modal', {
-   
+
     template: `
 	<!-- Modal -->
 	<div class="modal is-active">
@@ -7,7 +7,7 @@ Vue.component('modal', {
         <div class="modal-content">
             <!-- Any other Bulma elements you want -->
             <div class="box">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dicta, doloremque ea eligendi illum in, laboriosam laudantium magni nihil omnis quia repellat veritatis vitae. Ab consequuntur dolorum et eum sapiente!
+            <slot></slot>
             </div>
             
         </div>
