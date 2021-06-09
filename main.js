bdefd72e43f13657e31e724cdcd051ef00ada3a6
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-06-09 06:23:49 -0500

    refactoring

--- main.js
+++ main.js
@@ -13,19 +13,13 @@ Vue.component('message', {
         `<article class="message" v-show="isVisible">
             <div class="message-header">
                 {{title}}
-                <button type="button" @click="hideModal" class="delete" aria-label="delete">x</button>
+                <button type="button" @click="isVisible = false" class="delete" aria-label="delete">x</button>
             </div>
             
             <div class="message-body">
                 {{body}}
             </div>
-        </article>`,
-
-        methods: {
-        hideModal: function() {
-            this.isVisible = false;
-        }
-    }
+        </article>`
 
 
 
