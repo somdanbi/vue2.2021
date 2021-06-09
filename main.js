905fb3f3cdcd6ec88a9e34f17f7fd842e537a2fc
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-06-09 07:21:42 -0500

    component-exercise-2-modal
    display modal with a btn.
    config close that modal.

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-
+Vue.component('modal', {
+   
+    template: `
+	<!-- Modal -->
+	<div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <!-- Any other Bulma elements you want -->
+            <div class="box">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dicta, doloremque ea eligendi illum in, laboriosam laudantium magni nihil omnis quia repellat veritatis vitae. Ab consequuntur dolorum et eum sapiente!
+            </div>
+            
+        </div>
+    <button @click="$emit('close')" class="modal-close is-large" aria-label="close"></button>
+    </div>
+	`
+});
 
 new Vue({
-    el: '#root'
+    el: '#root',
+
+    data: {
+        showModal: false
+
+    }
 });
