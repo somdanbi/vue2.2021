70bc701b572e38aa2e277db01378dbb80a7ffd06
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-06-11 05:07:17 -0500

    showing tabs in Vue
    when I say vue I meant in the Tab vue(next to the console)

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+Vue.component('tabs',{
+    template: `
+    <div>
+    <div class="tabs">
+        <ul>
+            <li class="is-active"><a>Pictures</a></li>
+            <li><a>Music</a></li>
+            <li><a>Videos</a></li>
+            <li><a>Documents</a></li>
+        </ul>
+    </div>
+    
+    <div class="tab-details">
+        <slot></slot>
+    </div>
+    </div>
+    `,
+
+
+    data(){
+        return{ tabs:[]};
+    },
+
+    created(){
+        this.tab = this.children;
+    }
+});
+
+Vue.component('tab',{
+    template: `
+        <div><slot></slot></div>
+    `,
+
+    props: {
+        name: {required:true}
+    }
+});
+
 new Vue({
     el: '#root',
 });
