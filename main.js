05c06ccc175f7a0363d886f34e327020e146f95e
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-06-11 04:42:01 -0500

    11.1 Cleaning

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-Vue.component('modal', {
-
-    template: `
-	<!-- Modal -->
-	<div class="modal is-active">
-        <div class="modal-background"></div>
-        <div class="modal-content">
-            <!-- Any other Bulma elements you want -->
-            <div class="box">
-            <slot></slot>
-            </div>
-            
-        </div>
-    <button @click="$emit('close')" class="modal-close is-large" aria-label="close"></button>
-    </div>
-	`
-});
-
 new Vue({
     el: '#root',
-
-    data: {
-        showModal: false
-
-    }
 });
